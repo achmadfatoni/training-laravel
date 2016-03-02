552fe8e09946e591ab227fe307c073b09557e455
Achmad Fatoni <achmadfatony@gmail.com>
2016-03-02 19:22:52 +0700

    create biodata view

--- resources/views/biodata/create.blade.php
+++ resources/views/biodata/create.blade.php
@@ -1,20 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Biodata</title>
-</head>
-<body>
-	<form action="{{ url('biodata') }}" method="POST">
-		<label>Name</label>
-		<input type="text" name="name" />
+@extends('layout')
 
-		<label>Address</label>
-		<input type="text" name="address">
+@section('container')
+	<div class="row">
+		<div class="col-sm-6 col-sm-offset-3">
+		<form class="form-horizontal" action="{{ url('biodata') }}" method="POST">
+			<div class="form-group">
+				<label class="col-sm-2">Name</label>
+				<div class="col-sm-10">
+					<input type="text" name="name" class="form-control"/>
+				</div>
+			</div>
 
-		<label>Email</label>
-		<input type="email" name="email">
+			<div class="form-group">
+				<label class="col-sm-2">Address</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" name="address">
+				</div>
+			</div>
 
-		<button type="submit">Simpan</button>
-	</form>
-</body>
-</html>
+			<div class="form-group">
+				<label class="col-sm-2">Email</label>
+				<div class="col-sm-10">
+					<input type="email" class="form-control" name="email">
+				</div>
+			</div>
+
+			<div class="form-group">
+				<div class="col-sm-12">
+					<button type="submit" class="btn btn-primary pull-right">Simpan</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	</div>
+@stop
